76afce9b30ecc502e1aa073a03c0246340ebf932
Shubham More <shubham.more@iitgn.ac.in>
2024-08-17 14:25:25 +0530

    first commit

---
+++ ignition/modules/Token.js
@@ -0,0 +1,10 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+
+module.exports = buildModule("TokenModule", (m) => {
+
+
+  const tokenContract = m.contract("Token", []);
+
+  return { tokenContract };
+});
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import { Component } from 'react'
+import '../styles/globals.css'
+
+const MyApp=({Component,pageProps})=>(<Component{...pageProps}/>)
+
+export default MyApp
